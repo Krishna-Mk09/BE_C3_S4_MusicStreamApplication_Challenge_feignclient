b52bd763d7b490f08e6a24cf468fe07c4915c026
M V K <krishna.mugala9@gmail.com>
2022-12-08 14:18:36 +0530

    used eureka server and services are linked

---
+++ Eureka_server_Challenge/src/test/java/com/niit/Eureka_server_Challenge/EurekaServerChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niit.Eureka_server_Challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- springAPIGateWay/src/main/java/com/niit/jdp/springAPIGateWay/SpringApiGateWayApplication.java
+++ springAPIGateWay/src/main/java/com/niit/jdp/springAPIGateWay/SpringApiGateWayApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @SpringBootApplication
+@EnableEurekaClient
 public class SpringApiGateWayApplication {
 
 	public static void main(String[] args) {
@@ -21,10 +23,10 @@ public class SpringApiGateWayApplication {
 			return builder.routes()
 					.route(p->p
 							.path("/userservice/**")
-							.uri("http://UserAuthenticate:8081/"))
+							.uri("lb://UserAuthenticate/"))
 					.route(p->p
 							.path("/api/v1/**")
-							.uri("http://customer-service:8087/"))
+							.uri("lb://customer-service/"))
 					.build();
 		}
 }
